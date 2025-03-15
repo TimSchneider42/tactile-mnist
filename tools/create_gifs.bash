323438e8e6728a44994ffc311e595c5fadeafbc7
Tim Schneider <mail@tim-schneider.me>
2025-03-15 09:28:30 +0100

    Updated README and examples

---
+++ tools/create_gifs.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+ENVS=(
+  TactileMNIST-v0
+  Starstruck-v0
+)
+
+OUTPUT_DIR="$SCRIPT_DIR/../doc/img"
+rm -rf "${OUTPUT_DIR}"
+mkdir -p "${OUTPUT_DIR}"
+for env in "${ENVS[@]}"; do
+  ap-gym-create-env-gif "tactile_mnist:$env" "${OUTPUT_DIR}/$env.gif" &
+done
+
+wait
