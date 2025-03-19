4ebb12ec8a21527d961ddc88b65d024c7c46a492
Tim Schneider <tim@robot-learning.de>
2025-03-19 09:26:51 +0100

    Updated examples

--- example/tactile_mnist_env_example_vec.py
+++ example/tactile_mnist_env_example_vec.py
@@ -9,24 +9,20 @@ import ap_gym
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-resolution = (320, 240)
+# For the test dataset, use "TactileMNIST-test-v0"
+env = ap_gym.make_vec("tactile_mnist:TactileMNIST-train-v0", num_envs=4)
+obs, _ = env.reset(seed=0)
 
-env = ap_gym.make_vec(
-    "tactile_mnist:TactileMNIST-train-v0",  # For the test dataset, use "TactileMNIST-test-v0"
-    num_envs=4,
-    render_mode="rgb_array",
-    sensor_output_size=resolution,
-)
-
-fig, ax = plt.subplots(2, env.num_envs, squeeze=False)
-img_shape = (resolution[1], resolution[0], 3)
-camera_plot = [ax[0, i].imshow(np.zeros(img_shape)) for i in range(env.num_envs)]
-img_plot = [ax[1, i].imshow(np.zeros(img_shape)) for i in range(env.num_envs)]
+fig, axes = plt.subplots(2, env.num_envs, squeeze=False)
+img_plot = [
+    ax.imshow(np.zeros(env.single_observation_space["sensor_img"].shape))
+    for ax in axes[0]
+]
+camera_plot = [ax.imshow(img) for img, ax in zip(env.render(), axes[1])]
 plt.show(block=False)
 
+terminated = np.zeros(env.num_envs, dtype=bool)
 for s in range(100):
-    obs, _ = env.reset(seed=s)
-
     # Generate a circle trajectory
     angles = (
         np.arange(env.spec.max_episode_steps) / env.spec.max_episode_steps * 2 * np.pi
@@ -41,9 +37,12 @@ for s in range(100):
             "prediction": env.prediction_space.sample(),
         }
 
-        obs, _, _, _, info = env.step(action)
+        obs, _, terminated, _, info = env.step(action)
         camera_img = env.render()
         for i in range(env.num_envs):
             img_plot[i].set_data(obs["sensor_img"][i])
             camera_plot[i].set_data(camera_img[i])
         plt.pause(1 / env.metadata["render_fps"])
+    assert np.all(terminated)
+    # This will trigger the reset of the environments
+    obs, _, terminated, _, _ = env.step(env.action_space.sample())
