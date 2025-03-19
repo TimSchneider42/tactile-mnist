a2fc06ad5882bf6035777de1e6af3cc896d35e76
Tim Schneider <tim@robot-learning.de>
2025-03-19 09:26:51 +0100

    Updated examples

--- example/tactile_mnist_env_example.py
+++ example/tactile_mnist_env_example.py
@@ -9,23 +9,16 @@ import ap_gym
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-resolution = (320, 240)
-
-env = ap_gym.make(
-    "tactile_mnist:TactileMNIST-train-v0",  # For the test dataset, use "TactileMNIST-test-v0"
-    render_mode="rgb_array",
-    sensor_output_size=resolution,
-)
+# For the test dataset, use "TactileMNIST-test-v0"
+env = ap_gym.make("tactile_mnist:TactileMNIST-train-v0")
+obs, _ = env.reset(seed=0)
 
 fig, ax = plt.subplots(1, 2)
-img_shape = (resolution[1], resolution[0], 3)
-camera_plot = ax[0].imshow(np.zeros(img_shape))
-img_plot = ax[1].imshow(np.zeros(img_shape))
+camera_plot = ax[0].imshow(env.render())
+img_plot = ax[1].imshow(np.zeros(env.observation_space["sensor_img"].shape))
 plt.show(block=False)
 
-for s in range(100):
-    obs, _ = env.reset(seed=s)
-
+for _ in range(100):
     # Generate a circle trajectory
     angles = (
         np.arange(env.spec.max_episode_steps) / env.spec.max_episode_steps * 2 * np.pi
@@ -34,14 +27,17 @@ for s in range(100):
         [np.cos(angles), np.sin(angles), np.zeros_like(angles)], axis=-1
     )
 
+    terminated = False
     for p in target_trajectory:
         action = {
             "action": {"sensor_target_pos_rel": p - obs["sensor_pos"]},
             "prediction": env.prediction_space.sample(),
         }
 
-        obs, _, _, _, info = env.step(action)
+        obs, _, terminated, _, info = env.step(action)
         camera_img = env.render()
         img_plot.set_data(obs["sensor_img"])
         camera_plot.set_data(camera_img)
         plt.pause(1 / env.metadata["render_fps"])
+    assert terminated
+    obs, _ = env.reset()
