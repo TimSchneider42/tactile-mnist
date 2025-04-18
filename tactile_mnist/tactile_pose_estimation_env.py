93940f0f268446e10edae2874a5a10164a081304
Tim Schneider <tim@robot-learning.de>
2025-04-18 14:13:16 +0200

    Implemented frame determination for TactilePoseEstimationEnv

--- tactile_mnist/tactile_pose_estimation_env.py
+++ tactile_mnist/tactile_pose_estimation_env.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import functools
+from functools import partial
 from typing import (
     Literal,
+    TYPE_CHECKING,
 )
 
 import gymnasium as gym
@@ -14,10 +16,15 @@ from ap_gym import (
     ActivePerceptionVectorToSingleWrapper,
     MSELossFn,
 )
+from tactile_mnist import MeshDataPoint
 from .tactile_perception_vector_env import (
     TactilePerceptionVectorEnv,
     TactilePerceptionConfig,
+    ActType,
 )
+
+if TYPE_CHECKING:
+    from .tactile_perception_vector_env import ObsType
 
 
 class TactilePoseEstimationVectorEnv(
@@ -28,9 +35,15 @@ class TactilePoseEstimationVectorEnv(
         config: TactilePerceptionConfig,
         num_envs: int,
         render_mode: Literal["rgb_array", "human"] = "rgb_array",
+        frame_position_mode: Literal["model", "inertia_frame"] = "model",
+        frame_rotation_mode: Literal["model", "inertia_frame"] = "model",
     ):
         self.__compute_object_frame_cached = functools.lru_cache(maxsize=num_envs)(
-            self.__compute_object_frame
+            partial(
+                self.__compute_object_frame,
+                position_mode=frame_position_mode,
+                rotation_mode=frame_rotation_mode,
+            )
         )
 
         super().__init__(
@@ -86,9 +99,46 @@ class TactilePoseEstimationVectorEnv(
         return res
 
     @staticmethod
-    def __compute_object_frame(dp) -> Transformation:
-        # TODO
-        return Transformation()
+    def __compute_object_frame(
+        dp: MeshDataPoint,
+        position_mode: Literal["model", "center_of_mass"],
+        rotation_mode: Literal["model"],
+    ) -> Transformation:
+        if position_mode == "model":
+            position = np.zeros(3, dtype=np.float32)
+        elif position_mode == "inertia_frame":
+            position = dp.mesh.center_mass
+        else:
+            raise ValueError(f"Unknown frame position mode: {position_mode}")
+        if rotation_mode == "model":
+            rotation = Rotation.identity()
+        elif rotation_mode == "inertia_frame":
+            inertia_tensor = dp.mesh.moment_inertia
+            # Determine eigenvalues and eigenvectors of the X and Y components of the inertia tensor
+            inertia_components2d, inertia_vectors2d = np.linalg.eigh(
+                inertia_tensor[:2, :2]
+            )
+            # Axis in the XY plane along which the mass is most closely distributed
+            x_axis_idx = np.argmin(inertia_components2d)
+            x_axis_2d = inertia_vectors2d[x_axis_idx]
+
+            # The inertia frame is not unique in orientation, as it might be flipped 180 degrees. We always let the
+            # X-axis point towards the direction in which the object is the longest.
+            y_axis_2d = np.array([-x_axis_2d[1], x_axis_2d[0]], dtype=np.float32)
+            vertices_proj = dp.mesh.vertices[:, :2] @ y_axis_2d
+            max_val = np.max(vertices_proj)
+            min_val = np.min(vertices_proj)
+            if np.abs(min_val) > np.abs(max_val):
+                x_axis_2d = -x_axis_2d
+                y_axis_2d = -y_axis_2d
+
+            x_axis = np.concatenate([x_axis_2d, [0.0]], dtype=np.float32)
+            y_axis = np.concatenate([y_axis_2d, [0.0]], dtype=np.float32)
+            z_axis = np.array([0.0, 0.0, 1.0], dtype=np.float32)
+            rotation = Rotation.from_matrix(np.stack([x_axis, y_axis, z_axis], axis=1))
+        else:
+            raise ValueError(f"Unknown frame rotation mode: {rotation_mode}")
+        return Transformation(position, rotation)
 
     def _get_prediction_targets(self) -> np.ndarray:
         object_frames = Transformation.batch_concatenate(
@@ -109,13 +159,15 @@ class TactilePoseEstimationVectorEnv(
 def TactilePoseEstimationEnv(
     config: TactilePerceptionConfig,
     render_mode: Literal["rgb_array", "human"] = "rgb_array",
-) -> ActivePerceptionVectorToSingleWrapper[
-    "ObsType", "ActType", np.ndarray, np.ndarray
-]:
+    frame_position_mode: Literal["model", "inertia_frame"] = "model",
+    frame_rotation_mode: Literal["model", "inertia_frame"] = "model",
+) -> ActivePerceptionVectorToSingleWrapper["ObsType", ActType, np.ndarray, np.ndarray]:
     return ActivePerceptionVectorToSingleWrapper(
         TactilePoseEstimationVectorEnv(
             config,
             1,
             render_mode=render_mode,
+            frame_position_mode=frame_position_mode,
+            frame_rotation_mode=frame_rotation_mode,
         )
     )
