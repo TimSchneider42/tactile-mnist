7fb8c1c66e6efc4b87758e4453d7eea431481aca
Tim Schneider <tim@robot-learning.de>
2025-05-14 20:38:56 +0200

    Created new environment gifs

--- tools/create_gifs.bash
+++ tools/create_gifs.bash
@@ -5,15 +5,30 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ENVS=(
   TactileMNIST-v0
   TactileMNIST-test-v0
+  TactileMNIST-CycleGAN-v0
+  TactileMNIST-CycleGAN-test-v0
+  TactileMNIST-Depth-v0
+  TactileMNIST-Depth-test-v0
+  TactileMNISTVolume-v0
+  TactileMNISTVolume-test-v0
+  TactileMNISTVolume-CycleGAN-v0
+  TactileMNISTVolume-CycleGAN-test-v0
+  TactileMNISTVolume-Depth-v0
+  TactileMNISTVolume-Depth-test-v0
   Starstruck-v0
   Starstruck-test-v0
+  Starstruck-Depth-v0
+  Starstruck-Depth-test-v0
+  Toolbox-v0
+  Toolbox-Depth-v0
 )
 
 OUTPUT_DIR="$SCRIPT_DIR/../doc/img/env"
 rm -rf "${OUTPUT_DIR}"
 mkdir -p "${OUTPUT_DIR}"
 for env in "${ENVS[@]}"; do
-  ap-gym-create-env-gif "tactile_mnist:$env" "${OUTPUT_DIR}/$env.gif"
+  echo "Creating GIF for $env..."
+  ap-gym-create-env-vid "tactile_mnist:$env" "${OUTPUT_DIR}/$env.gif"
 done
 
 wait
