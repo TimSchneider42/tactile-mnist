Fixed normalization of pose estimation environments

tactile_mnist/tactile_perception_vector_env.py
@@ -296,6 +296,105 @@ class TactilePerceptionVectorEnv(
             sensor_poses.append(Transformation(position, rotation))
         return sensor_poses
 
+    @staticmethod
+    def _pre_process_dp(
+        dp: GenericMeshDataPoint, smallest_dimension_up: bool = False
+    ) -> GenericMeshDataPoint:
+        if smallest_dimension_up:
+            bb_oriented: trimesh.primitives.Box = dp.mesh.bounding_box_oriented
+            bb_rotation: Rotation = Rotation.from_matrix(bb_oriented.transform[:3, :3])
+            bb_vertices_local = bb_rotation.inv().apply(bb_oriented.vertices)
+            extents = np.max(bb_vertices_local, axis=0) - np.min(
+                bb_vertices_local, axis=0
+            )
+            axes = bb_rotation.as_matrix().T
+
+            extends_idx_sorted = np.argsort(extents)
+            z_axis = axes[extends_idx_sorted[0]]
+            if z_axis[-1] < 0:
+                z_axis = -z_axis
+            remaining_axes = axes[extends_idx_sorted[1:]]
+
+            if np.abs(remaining_axes[0, 0]) > np.abs(remaining_axes[1, 0]):
+                x_axis = remaining_axes[0]
+            else:
+                x_axis = remaining_axes[1]
+
+            y_axis = np.cross(z_axis, x_axis)
+
+            target_rotation = Rotation.from_matrix(
+                np.stack([x_axis, y_axis, z_axis], axis=-1)
+            )
+
+            mesh = dp.mesh.copy()
+            mesh.vertices = target_rotation.inv().apply(mesh.vertices)
+            dp = TransformedDataPoint(dp, mesh)
+        return dp
+
+    @staticmethod
+    def _get_random_object_pose_batch(
+        dp: GenericMeshDataPoint,
+        randomize_initial_pose: bool,
+        max_initial_angle_perturbation: float,
+        cell_size: tuple[float, float],
+        rotation_perturbation_norm: np.ndarray | None = None,
+        translation_perturbation_norm: np.ndarray | None = None,
+        np_random: np.random.Generator | None = None,
+    ) -> Transformation:
+        initial_pose = Transformation([[0, 0, -np.min(dp.mesh.vertices[:, 2])]])
+        if randomize_initial_pose:
+            if rotation_perturbation_norm is None:
+                rotation_perturbation_norm = np_random.uniform(
+                    low=0.0, high=1.0, size=(1,)
+                )
+            rotation_perturbation_euler = (
+                rotation_perturbation_norm * 2 - 1
+            ) * max_initial_angle_perturbation
+            rotation_perturbation = Rotation.from_euler(
+                "xyz",
+                np.concatenate(
+                    [
+                        np.zeros(
+                            (rotation_perturbation_euler.shape[0], 2), dtype=np.float32
+                        ),
+                        rotation_perturbation_euler[..., None],
+                    ],
+                    axis=-1,
+                ),
+            )
+            xy_pos = (
+                rotation_perturbation.as_matrix()[:, None]
+                @ dp.mesh.vertices[None, ..., None]
+            )[..., :2, 0]
+            xy_min = np.min(xy_pos, axis=1)
+            xy_max = np.max(xy_pos, axis=1)
+            margin = 0.01
+            low = -np.array(cell_size) / 2 + margin - xy_min
+            high = np.array(cell_size) / 2 - margin - xy_max
+            conflict = low > high
+            low[conflict] = high[conflict] = ((low + high) / 2)[conflict]
+            if translation_perturbation_norm is None:
+                translation_perturbation_norm = np_random.uniform(
+                    low=0.0, high=1.0, size=(1, 2)
+                )
+            translation_perturbation = (
+                translation_perturbation_norm * (high - low) + low
+            )
+            perturbation = Transformation(
+                np.concatenate(
+                    [
+                        translation_perturbation,
+                        np.zeros(
+                            (translation_perturbation.shape[0], 1), dtype=np.float32
+                        ),
+                    ],
+                    axis=-1,
+                ),
+                rotation_perturbation,
+            )
+            initial_pose *= perturbation
+        return initial_pose
+
     def __reset_partial(
         self, mask: Sequence[bool], options: dict[str, Any] | None = None
     ):
@@ -316,95 +415,18 @@ class TactilePerceptionVectorEnv(
                     else datapoint_idx[i]
                 )
 
-                new_dp: GenericMeshDataPoint = self.__datasets[i][idx]
-
-                if self.__config.smallest_dimension_up:
-                    mesh = new_dp.mesh.copy()
-                    bb_oriented: trimesh.primitives.Box = mesh.bounding_box_oriented
-                    bb_rotation: Rotation = Rotation.from_matrix(
-                        bb_oriented.transform[:3, :3]
-                    )
-                    bb_vertices_local = bb_rotation.inv().apply(bb_oriented.vertices)
-                    extents = np.max(bb_vertices_local, axis=0) - np.min(
-                        bb_vertices_local, axis=0
-                    )
-                    axes = bb_rotation.as_matrix().T
-
-                    extends_idx_sorted = np.argsort(extents)
-                    z_axis = axes[extends_idx_sorted[0]]
-                    if z_axis[-1] < 0:
-                        z_axis = -z_axis
-                    remaining_axes = axes[extends_idx_sorted[1:]]
-
-                    if np.abs(remaining_axes[0, 0]) > np.abs(remaining_axes[1, 0]):
-                        x_axis = remaining_axes[0]
-                    else:
-                        x_axis = remaining_axes[1]
-
-                    y_axis = np.cross(z_axis, x_axis)
-
-                    target_rotation = Rotation.from_matrix(
-                        np.stack([x_axis, y_axis, z_axis], axis=-1)
-                    )
-
-                    mesh.vertices = target_rotation.inv().apply(mesh.vertices)
-                    new_dp = TransformedDataPoint(new_dp, mesh)
-
-                current_datapoints_lst[i] = new_dp
+                current_datapoints_lst[i] = self._pre_process_dp(
+                    self.__datasets[i][idx],
+                    smallest_dimension_up=self.__config.smallest_dimension_up,
+                )
                 if initial_object_poses[i] is None:
-                    initial_pose = Transformation(
-                        [
-                            0,
-                            0,
-                            -np.min(current_datapoints_lst[i].mesh.vertices[:, 2]),
-                        ]
-                    )
-                    if self.__config.randomize_initial_object_pose:
-                        rotation_perturbation_euler = self.np_random.uniform(
-                            low=-self.__config.max_initial_angle_perturbation,
-                            high=self.__config.max_initial_angle_perturbation,
-                            size=(1,),
-                        )
-                        rotation_perturbation = Rotation.from_euler(
-                            "xyz",
-                            np.concatenate(
-                                [
-                                    np.zeros((2,), dtype=np.float32),
-                                    rotation_perturbation_euler,
-                                ]
-                            ),
-                        )
-                        xy_min = np.min(
-                            rotation_perturbation.apply(
-                                current_datapoints_lst[i].mesh.vertices
-                            )[:, :2],
-                            axis=0,
-                        )
-                        xy_max = np.max(
-                            rotation_perturbation.apply(
-                                current_datapoints_lst[i].mesh.vertices
-                            )[:, :2],
-                            axis=0,
-                        )
-                        margin = 0.01
-                        low = -np.array(self.__config.cell_size) / 2 + margin - xy_min
-                        high = np.array(self.__config.cell_size) / 2 - margin - xy_max
-                        conflict = low > high
-                        low[conflict] = high[conflict] = ((low + high) / 2)[conflict]
-                        translation_perturbation = self.np_random.uniform(
-                            low=low, high=high
-                        )
-                        perturbation = Transformation(
-                            np.concatenate(
-                                [
-                                    translation_perturbation,
-                                    np.zeros((1,), dtype=np.float32),
-                                ]
-                            ),
-                            rotation_perturbation,
-                        )
-                        initial_pose *= perturbation
-                    object_poses_lst[i] = initial_pose
+                    object_poses_lst[i] = self._get_random_object_pose_batch(
+                        current_datapoints_lst[i],
+                        self.__config.randomize_initial_object_pose,
+                        self.__config.max_initial_angle_perturbation,
+                        self.__config.cell_size,
+                        np_random=self.np_random,
+                    )[0]
                 else:
                     object_poses_lst[i] = initial_object_poses[i]
 

tactile_mnist/tactile_pose_estimation_env.py
@@ -21,7 +21,9 @@ from ap_gym import (
     MSELossFn,
 )
 from ap_gym.util import update_info_metrics_vec
-from tactile_mnist import MeshDataPoint
+from .mesh_dataset import MeshDataPoint, MeshDataset
+from .simple_mesh_dataset import SimpleMeshDataset
+from .util import get_dataset_stats
 from .tactile_perception_vector_env import (
     TactilePerceptionVectorEnv,
     TactilePerceptionConfig,
@@ -30,6 +32,71 @@ from .tactile_perception_vector_env import (
 
 if TYPE_CHECKING:
     from .tactile_perception_vector_env import ObsType
+
+
+def _compute_object_pose_distribution_stats(
+    idx: int,
+    ds: MeshDataset,
+    randomize_initial_object_pose: bool,
+    max_initial_angle_perturbation: float,
+    cell_size: tuple[float, float],
+    smallest_dimension_up: bool,
+    position_mode: Literal["model", "inertia_frame"],
+    rotation_mode: Literal["model", "inertia_frame"],
+    predict_position: bool,
+    predict_rotation: bool,
+) -> dict[str, float]:
+    ds = SimpleMeshDataset(ds)
+    dp = TactilePerceptionVectorEnv._pre_process_dp(
+        ds[idx], smallest_dimension_up=smallest_dimension_up
+    )
+    num_samples = 16
+    rotation_perturbations = np.repeat(np.linspace(0, 1, num_samples), 2, axis=0)
+    translation_perturbation_limits = np.repeat(
+        np.array([[0.0, 0.0], [1.0, 1.0]])[None], repeats=num_samples, axis=0
+    ).reshape((-1, 2))
+    poses = TactilePerceptionVectorEnv._get_random_object_pose_batch(
+        dp,
+        randomize_initial_object_pose,
+        max_initial_angle_perturbation,
+        cell_size,
+        rotation_perturbation_norm=rotation_perturbations,
+        translation_perturbation_norm=translation_perturbation_limits,
+    )
+    frame = TactilePoseEstimationVectorEnv._compute_object_frame(
+        dp, position_mode or "model", rotation_mode or "model"
+    )
+    targets_flat = TactilePoseEstimationVectorEnv._get_prediction_targets_batch(
+        poses,
+        frame,
+        predict_position,
+        predict_rotation,
+        cell_size,
+    )
+    targets = targets_flat.reshape((num_samples, 2, -1))
+    output = {}
+    if predict_position:
+        translation_targets = targets[..., :2]
+        # This should work as the transformation happening to the translation perturbation is just additive, so the
+        # distribution shape is not changed, only shifted.
+        translation_min = translation_targets[:, 0]
+        translation_max = translation_targets[:, 1]
+        translation_mean = np.mean((translation_min + translation_max) / 2, axis=0)
+        translation_var = np.mean((translation_max - translation_min) ** 2 / 12, axis=0)
+        output["mean_trans_0"] = translation_mean[0]
+        output["mean_trans_1"] = translation_mean[1]
+        output["var_trans_0"] = translation_var[0]
+        output["var_trans_1"] = translation_var[1]
+    if predict_rotation:
+        rotation_targets = targets[:, 0, -2:]
+        rotation_mean = np.mean(rotation_targets, axis=0)
+        rotation_var = np.var(rotation_targets, axis=0)
+        output["mean_rot_0"] = rotation_mean[0]
+        output["mean_rot_1"] = rotation_mean[1]
+        output["var_rot_0"] = rotation_var[0]
+        output["var_rot_1"] = rotation_var[1]
+
+    return output
 
 
 class TactilePoseEstimationVectorEnv(
@@ -46,7 +113,7 @@ class TactilePoseEstimationVectorEnv(
     ):
         self.__compute_object_frame_cached = functools.lru_cache(maxsize=num_envs)(
             partial(
-                self.__compute_object_frame,
+                self._compute_object_frame,
                 position_mode=frame_position_mode or "model",
                 rotation_mode=frame_rotation_mode or "model",
             )
@@ -55,14 +122,6 @@ class TactilePoseEstimationVectorEnv(
         self.__predict_position = frame_position_mode is not None
         self.__predict_rotation = frame_rotation_mode is not None
         target_dims = 0
-        if self.__predict_position:
-            target_dims += 2
-        if self.__predict_rotation:
-            target_dims += 2
-        if target_dims == 0:
-            raise ValueError(
-                "At least one of frame_position_mode or frame_rotation_mode must not be 'dont_use'"
-            )
 
         max_expected_translation_perturbation_norm = 0
         if config.perturb_object_pose:
@@ -76,22 +135,76 @@ class TactilePoseEstimationVectorEnv(
                 max_expected_translation_perturbation / (np.min(config.cell_size) / 2)
             )
 
-        prediction_bound = 1.0 + max_expected_translation_perturbation_norm
+        prediction_bound = []
 
+        if self.__predict_position:
+            target_dims += 2
+            prediction_bound += [1.0 + max_expected_translation_perturbation_norm] * 2
+        if self.__predict_rotation:
+            target_dims += 2
+            prediction_bound += [1.0] * 2
+        if target_dims == 0:
+            raise ValueError(
+                "At least one of frame_position_mode or frame_rotation_mode must not be 'dont_use'"
+            )
+
+        prediction_bound = np.asarray(prediction_bound)
         single_prediction_target_space = gym.spaces.Box(
             -prediction_bound, prediction_bound, shape=(target_dims,)
         )
+
+        kwargs = {
+            "randomize_initial_object_pose": config.randomize_initial_object_pose,
+            "max_initial_angle_perturbation": config.max_initial_angle_perturbation,
+            "cell_size": config.cell_size,
+            "smallest_dimension_up": config.smallest_dimension_up,
+            "position_mode": frame_position_mode,
+            "rotation_mode": frame_rotation_mode,
+            "predict_position": self.__predict_position,
+            "predict_rotation": self.__predict_rotation,
+        }
+
+        statistics = get_dataset_stats(
+            config.dataset,
+            "pose",
+            _compute_object_pose_distribution_stats,
+            kwargs,
+        )
+
+        var = np.array(())
+        if self.__predict_position:
+            var = np.concatenate(
+                [
+                    var,
+                    [
+                        statistics["mean_trans_0"]["std"] ** 2
+                        + statistics["var_trans_0"]["mean"],
+                        statistics["mean_trans_1"]["std"] ** 2
+                        + statistics["var_trans_1"]["mean"],
+                    ],
+                ]
+            )
+        if self.__predict_rotation:
+            var = np.concatenate(
+                [
+                    var,
+                    [
+                        statistics["mean_rot_0"]["std"] ** 2
+                        + statistics["var_rot_0"]["mean"],
+                        statistics["mean_rot_1"]["std"] ** 2
+                        + statistics["var_rot_1"]["mean"],
+                    ],
+                ]
+            )
 
         super().__init__(
             config,
             num_envs,
             # We allow for more than [-1, 1] range to account for objects moving beyond the platform during the episode
-            single_prediction_space=gym.spaces.Box(
-                -prediction_bound, prediction_bound, shape=(target_dims,)
-            ),
+            single_prediction_space=single_prediction_target_space,
             single_prediction_target_space=single_prediction_target_space,
             # Assuming uniform distribution over the prediction target space
-            loss_fn=MSELossFn(target_std=2 * prediction_bound / np.sqrt(12)).normalized,
+            loss_fn=MSELossFn(target_std=np.sqrt(var)).normalized,
             render_mode=render_mode,
         )
         self.__metrics: dict[str, tuple[deque[float], ...]] | None = None
@@ -208,7 +321,7 @@ class TactilePoseEstimationVectorEnv(
         return obs, action_reward, terminated, truncated, info, labels
 
     @staticmethod
-    def __compute_object_frame(
+    def _compute_object_frame(
         dp: MeshDataPoint,
         position_mode: Literal["model", "inertia_frame"],
         rotation_mode: Literal["model", "inertia_frame"],
@@ -249,21 +362,25 @@ class TactilePoseEstimationVectorEnv(
             raise ValueError(f"Unknown frame rotation mode: {rotation_mode}")
         return Transformation(position, rotation)
 
-    def _get_prediction_targets(self) -> np.ndarray:
-        object_frames = Transformation.batch_concatenate(
-            [self.__compute_object_frame_cached(dp) for dp in self.current_data_points]
-        )
-        object_frames_world = self.current_object_poses_platform_frame * object_frames
+    @staticmethod
+    def _get_prediction_targets_batch(
+        object_poses: Transformation,
+        object_frames: Transformation,
+        predict_position: bool,
+        predict_rotation: bool,
+        cell_size: tuple[float, float],
+    ) -> np.ndarray:
+        object_frames_world = object_poses * object_frames
         object_position_2d = object_frames_world.translation[..., :2]
 
         target = []
-        if self.__predict_position:
+        if predict_position:
             object_position_2d_norm = object_position_2d / (
-                np.array(self.config.cell_size, dtype=np.float32) / 2
+                np.array(cell_size, dtype=np.float32) / 2
             )
             target.append(object_position_2d_norm)
 
-        if self.__predict_rotation:
+        if predict_rotation:
             object_x_axis_2d = object_frames_world.rotation.as_matrix()[..., :2, 0]
             object_x_axis_2d = object_x_axis_2d / np.linalg.norm(
                 object_x_axis_2d, axis=-1, keepdims=True
@@ -271,6 +388,18 @@ class TactilePoseEstimationVectorEnv(
             target.append(object_x_axis_2d)
 
         return np.concatenate(target, axis=-1)
+
+    def _get_prediction_targets(self) -> np.ndarray:
+        object_frames = Transformation.batch_concatenate(
+            [self.__compute_object_frame_cached(dp) for dp in self.current_data_points]
+        )
+        return self._get_prediction_targets_batch(
+            self.current_object_poses_platform_frame,
+            object_frames,
+            self.__predict_position,
+            self.__predict_rotation,
+            self.config.cell_size,
+        )
 
 
 def TactilePoseEstimationEnv(

tactile_mnist/tactile_volume_estimation_env.py
@@ -1,36 +1,30 @@
 from __future__ import annotations
 
 import functools
-import json
 import logging
-import multiprocessing.pool
 from collections import deque, defaultdict
-from functools import partial
 from typing import (
     Literal,
     TYPE_CHECKING,
     Any,
 )
 
-import datasets
-import filelock
 import gymnasium as gym
 import numpy as np
-import tqdm
 
 from ap_gym import (
     ActivePerceptionVectorToSingleWrapper,
     MSELossFn,
 )
 from ap_gym.util import update_info_metrics_vec
-from .constants import CACHE_BASE_DIR
-from .mesh_dataset import MeshDataPoint
+from .mesh_dataset import MeshDataPoint, MeshDataset
 from .simple_mesh_dataset import SimpleMeshDataset
 from .tactile_perception_vector_env import (
     TactilePerceptionVectorEnv,
     TactilePerceptionConfig,
     ActType,
 )
+from .util import get_dataset_stats
 
 if TYPE_CHECKING:
     from .tactile_perception_vector_env import ObsType
@@ -38,8 +32,8 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def _compute_object_volume_idx(idx: int, ds: datasets.Dataset):
-    return SimpleMeshDataset(ds)[idx].mesh.volume
+def _compute_object_volume_idx(idx: int, ds: MeshDataset):
+    return {"volume": SimpleMeshDataset(ds)[idx].mesh.volume}
 
 
 class TactileVolumeEstimationVectorEnv(
@@ -56,68 +50,13 @@ class TactileVolumeEstimationVectorEnv(
             self.__compute_object_volume
         )
 
-        cache_dir = CACHE_BASE_DIR / "volume_estimation"
-        cache_dir.mkdir(parents=True, exist_ok=True)
-        ds_fingerprint = config.dataset.huggingface_dataset._fingerprint
-        cache_file = cache_dir / f"{ds_fingerprint}.json"
-        self.__mean_volume = self.__std_volume = self.__min_volume = (
-            self.__max_volume
-        ) = None
-        with filelock.FileLock(cache_dir / f"{ds_fingerprint}.lock"):
-            if cache_file.exists():
-                try:
-                    with cache_file.open() as f:
-                        data = json.load(f)
-                    self.__mean_volume = data["mean_volume"]
-                    self.__std_volume = data["std_volume"]
-                    self.__min_volume = data["min_volume"]
-                    self.__max_volume = data["max_volume"]
-                except Exception as ex:
-                    logger.warning(
-                        f"Loading volume statistics from cache failed with the following exception: {ex}"
-                    )
-            if any(
-                m is None
-                for m in [
-                    self.__mean_volume,
-                    self.__std_volume,
-                    self.__min_volume,
-                    self.__max_volume,
-                ]
-            ):
-                print(
-                    "Computing object volumes for normalization (the results will be cached)..."
-                )
-                with multiprocessing.pool.Pool(
-                    processes=min(multiprocessing.cpu_count(), 8)
-                ) as pool:
-                    volumes = list(
-                        tqdm.tqdm(
-                            pool.imap_unordered(
-                                partial(
-                                    _compute_object_volume_idx,
-                                    ds=config.dataset.huggingface_dataset,
-                                ),
-                                range(len(config.dataset)),
-                            ),
-                            total=len(config.dataset),
-                        )
-                    )
-
-                self.__mean_volume = np.mean(volumes)
-                self.__std_volume = np.std(volumes)
-                self.__min_volume = np.min(volumes)
-                self.__max_volume = np.max(volumes)
-                with cache_file.open("w") as f:
-                    json.dump(
-                        {
-                            "mean_volume": self.__mean_volume,
-                            "std_volume": self.__std_volume,
-                            "min_volume": self.__min_volume,
-                            "max_volume": self.__max_volume,
-                        },
-                        f,
-                    )
+        statistics = get_dataset_stats(
+            config.dataset, "volume", _compute_object_volume_idx
+        )
+        self.__mean_volume = statistics["volume"]["mean"]
+        self.__std_volume = statistics["volume"]["std"]
+        self.__min_volume = statistics["volume"]["min"]
+        self.__max_volume = statistics["volume"]["max"]
 
         pred_space = gym.spaces.Box(
             self.__normalize_volume(self.__min_volume),